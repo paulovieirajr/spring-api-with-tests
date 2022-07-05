0cf82763507887b7ece4328b28493282e5bbe022
Junior Vieira <junior.vieira.1990@gmail.com>
2022-07-05 20:43:16 -0300

    Project Created

---
+++ src/test/java/io/github/vieirajunior90/apiwithtests/ApiWithTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vieirajunior90.apiwithtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWithTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
