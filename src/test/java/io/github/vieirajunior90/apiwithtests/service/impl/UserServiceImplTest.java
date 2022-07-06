941e99ed907a4815f1aed54549326b0ef202a050
Junior Vieira <junior.vieira.1990@gmail.com>
2022-07-06 19:36:21 -0300

    UserServiceImplTest findById ObjectNotFound

--- src/test/java/io/github/vieirajunior90/apiwithtests/service/impl/UserServiceImplTest.java
+++ src/test/java/io/github/vieirajunior90/apiwithtests/service/impl/UserServiceImplTest.java
@@ -3,6 +3,8 @@ package io.github.vieirajunior90.apiwithtests.service.impl;
 import io.github.vieirajunior90.apiwithtests.domain.User;
 import io.github.vieirajunior90.apiwithtests.domain.dto.UserDto;
 import io.github.vieirajunior90.apiwithtests.repository.UserRepository;
+import io.github.vieirajunior90.apiwithtests.service.exception.ObjectNotFoundException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -12,8 +14,7 @@ import org.modelmapper.ModelMapper;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -23,6 +24,7 @@ class UserServiceImplTest {
     public static final String NAME = "John";
     public static final String EMAIL = "john@email.com";
     public static final String PASSWORD = "123";
+    public static final String USER_NOT_FOUND = "User not found";
 
     @InjectMocks
     private UserServiceImpl service;
@@ -54,6 +56,19 @@ class UserServiceImplTest {
         assertEquals(ID, response.getId());
         assertEquals(NAME, response.getName());
         assertEquals(EMAIL, response.getEmail());
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnObjectNotFound() {
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(USER_NOT_FOUND));
+
+        try {
+            service.findById(ID);
+        }
+        catch (Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(USER_NOT_FOUND, ex.getMessage());
+        }
     }
 
     @Test
