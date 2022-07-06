d46aadb4742d63cd4761de69f46e050c7d1d970c
Junior Vieira <junior.vieira.1990@gmail.com>
2022-07-05 21:40:09 -0300

    findById implemented with UserService

--- src/main/java/io/github/vieirajunior90/apiwithtests/controller/UserController.java
+++ src/main/java/io/github/vieirajunior90/apiwithtests/controller/UserController.java
@@ -1,6 +1,7 @@
 package io.github.vieirajunior90.apiwithtests.controller;
 
 import io.github.vieirajunior90.apiwithtests.domain.User;
+import io.github.vieirajunior90.apiwithtests.service.impl.UserServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    final UserServiceImpl userService;
+
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok(new User(1, "John Doe", "john@gmail.com", "123"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
