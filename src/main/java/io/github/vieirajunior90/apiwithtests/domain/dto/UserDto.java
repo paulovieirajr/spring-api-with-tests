56d4d5412bf526f08faf7afc09e442023c27e6de
Junior Vieira <junior.vieira.1990@gmail.com>
2022-07-06 19:21:43 -0300

    AllArgsConstructor added

--- src/main/java/io/github/vieirajunior90/apiwithtests/domain/dto/UserDto.java
+++ src/main/java/io/github/vieirajunior90/apiwithtests/domain/dto/UserDto.java
@@ -2,11 +2,13 @@ package io.github.vieirajunior90.apiwithtests.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 public class UserDto implements Serializable {
     private Integer id;
     private String name;
