d0e5deed137721f8b004298d99d1c4c41432ccaa
Junior Vieira <junior.vieira.1990@gmail.com>
2022-07-08 12:02:43 -0300

    ResourceExceptionHandlerTest dataIntegrityViolationException

--- src/test/java/io/github/vieirajunior90/apiwithtests/controller/exception/ResourceExceptionHandlerTest.java
+++ src/test/java/io/github/vieirajunior90/apiwithtests/controller/exception/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package io.github.vieirajunior90.apiwithtests.controller.exception;
 
+import io.github.vieirajunior90.apiwithtests.service.exception.DataIntegrityViolationException;
 import io.github.vieirajunior90.apiwithtests.service.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,12 +11,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
 
     public static final String USER_NOT_FOUND = "User not found";
+    public static final String EMAIL_ALREADY_EXISTS = "Email already exists";
     @InjectMocks
     private ResourceExceptionHandler exceptionHandler;
 
@@ -42,6 +48,22 @@ class ResourceExceptionHandlerTest {
     }
 
     @Test
-    void dataIntegrityViolation() {
+    void whenDataIntegrityViolationExceptionThenReturnAResponseEntity() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dataIntegrityViolation(
+                        new DataIntegrityViolationException(EMAIL_ALREADY_EXISTS),
+                        new MockHttpServletRequest());
+
+        var dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(EMAIL_ALREADY_EXISTS, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
+        assertEquals(dtf.format(LocalDateTime.now()), response.getBody().getTimestamp());
+        assertNotEquals("/user/2", response.getBody().getPath());
     }
 }
